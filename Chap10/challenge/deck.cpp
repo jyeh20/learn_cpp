a8f0c63767a5330709ca213b127d7f2b867c8710
jyeh20 <jmyeh51@gmail.com>
2022-07-16 11:37:01 -0700

    Added new_deck, print_string, and get_unshuffled_deck methods

--- Chap10/challenge/deck.cpp
+++ Chap10/challenge/deck.cpp
@@ -1,9 +1,15 @@
 #include <cstdio>
+#include <cstring>
+
+constexpr char SUITS[] = "SHDC";
+constexpr char VALUES[] = "A123456789TJQK";
 
 struct Card {
   char suit : 4;
   int value : 4;
 };
+
+Card* get_unshuffled_deck();
 
 class Deck {
   int _cards_remaining;
@@ -18,4 +24,38 @@ class Deck {
     const Card & deal_card();
     void new_deck();
     void new_deck(int n);
+    void print_string();
 };
+
+void Deck::new_deck() {
+  _cards = get_unshuffled_deck();
+}
+
+void Deck::print_string() {
+  for (int i = 0; i < sizeof(_cards); ++i) {
+    Card c = _cards[i];
+    printf("%c%c ", c.suit, c.value);
+  }
+}
+
+// Helpers
+
+Card * get_unshuffled_deck() {
+  Card deck[52];
+  int index = 0;
+  for (int i = 0; i < strlen(SUITS); ++i) {
+    char suit = SUITS[i];
+    for (int j = 0; j < strlen(VALUES); ++j) {
+      Card card;
+      card.suit = suit;
+      card.value = VALUES[j];
+      deck[++index] = card;
+    }
+  }
+  return deck;
+}
+
+int main() {
+  get_unshuffled_deck();
+  return 1;
+}
